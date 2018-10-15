b6b4d6f74cb58935461ad262eaca468a84aad37c
tvtrung <tvtrung1101@gmail.com>
2018-10-15 17:39:53 +0700

    [15-10-2018] fix detail post

--- resources/views/page/main/detail-post.blade.php
+++ resources/views/page/main/detail-post.blade.php
@@ -45,6 +45,7 @@
 				@if(isset($configs_data['fb_social']['fb_app_id']) && isset($configs_data['fb_social']['fb_app_id']) != null)
 				<div class="fb-comments" data-href="{{URL::current()}}" data-numposts="5" data-order-by="reverse_time"></div>
 				@endif
+				@if(isset($row_relative_post))
 				<div class="clearfix"></div><hr>
 				<div class="row">
 					<div class="col-md-12">
@@ -60,30 +61,28 @@
 						<div class="posts-carousel">
 							<div class="row" style="margin-left: -5px;margin-right: -5px;">
 								<div class="owl-carousel owl-carousel-1">
-									@if(isset($row_relative_post))
-										@foreach($row_relative_post as $item)
-										<div class="item">
-											<div class="box-post-item-carousel">
-												<div class="post-item-1">
-													<div class="img">
-														<a href="{{$url_post[$item->id]}}"><img src="/style/images/news3.jpg" class="width100" alt=""></a>
-													</div>
-													<div class="title">
-														<a href="{{$url_post[$item->id]}}">{!!$item->title!!}</a>
-													</div>
-													<div class="info">
-														<i class="fa fa-user" aria-hidden="true"></i> Admin<span style="margin: 0 5px;">|</span>
-														<i class="fa fa-calendar" aria-hidden="true"></i> Ngày {{date('d/m/Y',strtotime($item->created_at))}}<span style="margin: 0 5px;">|</span>
-														<i class="fa fa-eye" aria-hidden="true"></i> {{$item->view}}
-													</div>
-													<div class="link">
-														<a href="{{$url_post[$item->id]}}">Xem thêm <i class="fa fa-angle-double-right" aria-hidden="true"></i></a>
-													</div>
-												</div>
+								@foreach($row_relative_post as $item)
+								<div class="item">
+									<div class="box-post-item-carousel">
+										<div class="post-item-1">
+											<div class="img">
+												<a href="{{$url_post[$item->id]}}"><img src="/style/images/news3.jpg" class="width100" alt=""></a>
+											</div>
+											<div class="title">
+												<a href="{{$url_post[$item->id]}}">{!!$item->title!!}</a>
+											</div>
+											<div class="info">
+												<i class="fa fa-user" aria-hidden="true"></i> Admin<span style="margin: 0 5px;">|</span>
+												<i class="fa fa-calendar" aria-hidden="true"></i> Ngày {{date('d/m/Y',strtotime($item->created_at))}}<span style="margin: 0 5px;">|</span>
+												<i class="fa fa-eye" aria-hidden="true"></i> {{$item->view}}
+											</div>
+											<div class="link">
+												<a href="{{$url_post[$item->id]}}">Xem thêm <i class="fa fa-angle-double-right" aria-hidden="true"></i></a>
 											</div>
 										</div>
-										@endforeach
-									@endif
+									</div>
+								</div>
+								@endforeach
 								</div>
 							</div>
 							<div class="pre-next text-center">
@@ -93,6 +92,7 @@
 						</div>
 					</div>
 				</div>
+				@endif
 			</div>
 			<div class="col-md-4">
 				@include('page.main.sidebar')
